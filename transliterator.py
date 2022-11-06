a3953aabe0cab4848705627ef18bf1a1fbc1ba1f
Mausam Kumar <70390027+mausam3407@users.noreply.github.com>
2022-11-06 10:47:02 +0530

    Update transliterator.py

--- transliterator.py
+++ transliterator.py
@@ -13,7 +13,7 @@ model = Translator(UNIT, en, hi)
 model.load_weights('metadata/weights_2')
 st.title("Roman Devanagari Transliterator")
 
-input = st.text_input("Input")
+input = st.text_input("Input", value = "Dukh")
 
 st.write("Output")
 
